9ed000d7390019ffa05e89c64e1198b41d63b862
alex-toy <alexei.80@hotmail.fr>
2024-02-02 19:35:34 +0100

    die class

---
+++ die.js
@@ -0,0 +1,21 @@
+//-------------------------------------------------------
+function Die(elementId, positionInView){
+  this.diceValue = 0;
+  this.elementId = elementId;
+  this.positionInView = positionInView;
+  let d = document.getElementById(elementId);
+  d.innerHTML = loadPage('dice.html');
+}
+
+Die.prototype.launchDice = function(){
+  this.diceValue = Math.floor(Math.random() * 6 ) + 1;
+  this.displayDiceResult();
+  return this.diceValue;
+}
+
+Die.prototype.displayDiceResult = function(){
+  for (var i=1; i<=6; i++) {
+    var query = `#d${this.positionInView} .dice${i}`;
+    (document.querySelector(query)).style.display = i == this.diceValue ? 'block' : 'none';
+  }
+}
